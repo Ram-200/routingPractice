0bad3b39afef982acab3ba202f1d432d92d83783
rambali <rrambali780@gmail.com>
2023-12-17 11:57:44 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="home-img"
+    />
+  </div>
+)
+export default Home
